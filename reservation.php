cf052bcd0001143bac469b316553fa0da44a0d0a
unknown <galli.johanna.g2@gmail.com>
2020-11-24 15:10:18 +0100

    form reservation complet

--- reservation.php
+++ reservation.php
@@ -58,7 +58,6 @@
     <!-------------------- HEADER END -------------------->
 
     <!-------------------- MAIN -------------------->
-
     <!--Formulaire de contact-->
     <h3 class="text-center pt-3 pb-3 font-weight-bold">Réservation en ligne :</h3>
 
@@ -67,30 +66,46 @@
         <!-- Informations client -->
         <div class="infoClient mb-3">
             <h4 class="pb-3">Informations client :</h4>
+
+            <!-- Nom -->
             <div class="inputSize">
                 <input type="text" maxlength="50" name="name" placeholder="Nom" class="inputs" id="inputName">
                 <p class="error" id="inputNameError"></p>
             </div>
+
+            <!-- Prénom -->
             <div class="inputSize">
                 <input type="text" maxlength="50" name="forename" placeholder="Prénom" class="inputs" id="inputForename">
                 <p class="error" id="inputForenameError"></p>
             </div>
+
+            <!-- Adresse postale -->
             <div class="inputSize">
                 <input type="text" maxlength="150" name="adressPostal" placeholder="Adresse postale" class="inputs" id="inputAdressPostal">
                 <p class="error" id="inputAdressPostalError"></p>
             </div>
-            <div class="inputSize">
-                <input type="text" maxlength="7" name="codepostal" placeholder="Code postal" class="inputs" id="inputCodePostal">
-                <p class="error" id="inputCodePostalError"></p>
+
+            <!-- Code postal + ville -->
+            <div class="inputSize row">
+                <!-- code postal -->
+                <div class="col-4">
+                    <input type="text" maxlength="7" name="codepostal" placeholder="Code postal" class="inputs" id="inputCodePostal">
+                    <p class="error" id="inputCodePostalError"></p>
+                </div>
+                <!-- ville -->
+                <div class="col-8">
+                    <input name="ville" placeholder="Ville" maxlength="60" class="inputs" id="inputVille"></input>
+                    <p class="error" id="inputVilleError"></p>
+                </div>
             </div>
-            <div class="inputSize">
-                <input name="ville" placeholder="Ville" maxlength="60" class="inputs" id="inputVille"></input>
-                <p class="error" id="inputVilleError"></p>
-            </div>
+
+            <!-- téléphone -->
             <div class="inputSize">
                 <input name="phone" placeholder="Téléphone" maxlength="15" class="inputs" id="inputPhone"></input>
                 <p class="error" id="inputPhoneError"></p>
             </div>
+
+            <!-- Mail -->
             <div class="inputSize">
                 <input name="email" placeholder="E-mail" maxlength="30" class="inputs" id="inputMail"></input>
                 <p class="error" id="inputMailError"></p>
@@ -98,20 +113,89 @@
         </div>
 
         <!-- Voyage -->
-        <div class="infoClient">
-            <h4>Voyage :</h4>
-
+        <div class="infoVoyage mb-3">
+            <h4>Informations voyage :</h4>
             <!-- Départ -->
             <h5>Départ :</h5>
+
+            <!-- Adresse postale -->
             <div class="inputSize">
-                <input name="ville" placeholder="Ville" maxlength="60" class="inputs" id="inputVille"></input>
-                <p class="error" id="inputVilleError"></p>
+                <input type="text" maxlength="150" name="adressPostal" placeholder="Adresse postale" class="inputs" id="inputAdressPostal">
+                <p class="error" id="inputAdressPostalError"></p>
+            </div>
+
+            <!-- Ville + code postal -->
+            <div class="inputSize row">
+                <!-- code postal -->
+                <div class="col-4">
+                    <input type="text" maxlength="7" name="codepostal" placeholder="Code postal" class="inputs" id="inputCodePostal">
+                    <p class="error" id="inputCodePostalError"></p>
+                </div>
+                <!-- ville -->
+                <div class="col-8">
+                    <input name="ville" placeholder="Ville" maxlength="60" class="inputs" id="inputVille"></input>
+                    <p class="error" id="inputVilleError"></p>
+                </div>
+            </div>
+
+            <!-- Date + heure -->
+            <div class="inputSize row">
+                <!-- date -->
+                <div class="col-5">
+                    <input type="date" name="date" class="inputs" id="inputDate">
+                    <p class="error" id="inputDateError"></p>
+                </div>
+                <!-- heure -->
+                <div class="col-4">
+                    <input type="time" step="600" name="hour" class="inputs" id="inputHour">
+                    <p class="error" id="inputHourError"></p>
+                </div>
             </div>
 
             <!-- Arrivée -->
             <h5>Arrivée :</h5>
+
+            <!-- Adresse postale -->
+            <div class="inputSize">
+                <input type="text" maxlength="150" name="adressPostal" placeholder="Adresse postale" class="inputs" id="inputAdressPostal">
+                <p class="error" id="inputAdressPostalError"></p>
+            </div>
+
+            <!-- Ville + code postal -->
+            <div class="inputSize row">
+                <!-- code postal -->
+                <div class="col-4">
+                    <input type="text" maxlength="7" name="codepostal" placeholder="Code postal" class="inputs" id="inputCodePostal">
+                    <p class="error" id="inputCodePostalError"></p>
+                </div>
+                <!-- ville -->
+                <div class="col-8">
+                    <input name="ville" placeholder="Ville" maxlength="60" class="inputs" id="inputVille"></input>
+                    <p class="error" id="inputVilleError"></p>
+                </div>
+            </div>
         </div>
 
+        <!-- Voyage -->
+        <div class="infoVoyage mb-3">
+            <h4>Informations complémentaires :</h4>
+
+            <!-- Nombre de passagers -->
+            <div class="inputSize">
+                <input type="number" min="1" max="7" name="numberOfPassager" class="inputs" id="inputnumberOfPassager">
+                <p class="error" id="inputNumberPassagerError"></p>
+            </div>
+            <!-- Nombre de bagages -->
+            <div class="inputSize">
+                <input type="number" min="0" max="10" name="numberOfBag" class="inputs" id="inputnumberOfBag">
+                <p class="error" id="inputnumberOfBagError"></p>
+            </div>
+            <!-- Infos complémentaires -->
+            <div class="inputSize">
+            <textarea name="message" placeholder="Message" maxlength="3000" class="inputs" id="inputMessage"></textarea>
+                <p class="error" id="inputnumberOfBagError"></p>
+            </div>
+        </div>
 
 
 
@@ -123,20 +207,20 @@
     </form>
     <!-------------------- MAIN END -------------------->
 
-    <!-------------------- FOOTER -------------------->
-    <div class="footer row">
-        <!-- gauche -->
+    <!-------------------- FOOTER ----------
+    <div class="footer row">---------->
+    <!-- gauche
         <div class="m-auto col-12 order-3 col-sm-4 order-sm-1">
             <p>
                 Design : <a href="http://johanna-galli.fr">Johanna Galli</a>
             </p>
             <a href="#">Mentions légales</a>
-        </div>
+        </div> -->
 
-        <!-- milieu -->
+    <!-- milieu 
         <img class="col-12 order-2 col-sm-4 order-sm-2" src="asset/img/logo-ATM-2.png" alt="logo ATM">
-
-        <!-- droite -->
+-->
+    <!-- droite
         <div class="m-auto col-12 order-1 col-sm-4 order-sm-3">
             <p>
                 CONTACT :
@@ -145,7 +229,7 @@
                 adresse mail
             </p>
         </div>
-    </div>
+    </div> -->
     <!-------------------- FOOTER END -------------------->
 </body>
 
